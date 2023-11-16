f2a495a766087895788f71b916046644d32d62cd
brunoodaniel <brunodanielsilva2002@gmail.com>
2023-11-16 15:02:30 -0300

    começando ex03 - treino

--- Aula04 - Formulário/script - aula04.js
+++ Aula04 - Formulário/script - aula04.js
@@ -1,14 +1,36 @@
 function submitForm() {
-    var nome = document.getElementById('nome').value;
-    var email = document.getElementById('email').value;
-    var msg = document.getElementById('mensagem').value;
+    var nome = document.getElementById('nome');
+    var email = document.getElementById('email');
+    var msg = document.getElementById('mensagem');
 
-    if (nome === '' || email === '' || msg === '') {
+    if (nome.value === '' || email.value === '' || msg.value === '') {
         alert('Por favor, preencha todos os campos.');
-    } else if (!isValidEmail(email)) {
+        if (nome.value === '') {
+            nome.style.borderColor = 'red';
+        } else {
+            nome.style.borderColor = 'azure';
+        }
+        if (email.value === '') {
+            email.style.borderColor = 'red';
+        } else if (!isValidEmail(email.value)) {
+            alert('Por favor, insira um endereço de e-mail válido.');
+            email.style.borderColor = 'red';
+        } else {
+            email.style.borderColor = 'azure';
+        }
+        if (msg.value === '') {
+            msg.style.borderColor = 'red';
+        } else {
+            msg.style.borderColor = 'azure';
+        }
+    } else if (!isValidEmail(email.value)) {
         alert('Por favor, insira um endereço de e-mail válido.');
+        email.style.borderColor = 'red';
     } else {
-        alert(nome + ' ' + email + ' ' + msg + ' ');
+        alert("Nome: " + nome.value + "\nEmail: " + email.value + "\nMensagem: " + msg.value);
+        nome.style.borderColor = 'azure';
+        email.style.borderColor = 'azure';
+        msg.style.borderColor = 'azure';
     }
 }
 
