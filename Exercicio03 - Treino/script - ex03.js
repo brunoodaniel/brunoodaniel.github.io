5619fa30419f3d9a1e8b0f58b982011823cf6d1c
brunoodaniel <brunodanielsilva2002@gmail.com>
2023-11-24 14:47:55 -0300

    carrossel finalizado

---
+++ Exercicio03 - Treino/script - ex03.js
@@ -0,0 +1,23 @@
+let currentIndex = 0;
+  const items = document.querySelectorAll('.carousel-item');
+
+  function changeSlide(n) {
+    currentIndex += n;
+    showSlides();
+  }
+
+  function showSlides() {
+    const totalItems = items.length;
+
+    if (currentIndex < 0) {
+      currentIndex = totalItems - 1;
+    } else if (currentIndex >= totalItems) {
+      currentIndex = 0;
+    }
+
+    items.forEach(item => item.classList.remove('active'));
+    items[currentIndex].classList.add('active');
+  }
+
+  // Exibir a primeira imagem ao carregar a página
+  showSlides();
